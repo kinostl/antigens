0bff7db9b5137e636aaec3cfd33aeb690469ecbf
Minty <kinostl@gmail.com>
2020-06-11 04:17:41 -0400

    Updates

---
+++ precache-manifest.824d2f6aec028398a72f475b3c0c3623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f3a81c0f7485202c6a3540b581e1d6",
+    "url": "/AntiGens/index.html"
+  },
+  {
+    "revision": "3c4c4ca54581bcda06f7",
+    "url": "/AntiGens/static/css/2.58c4053f.chunk.css"
+  },
+  {
+    "revision": "3c4c4ca54581bcda06f7",
+    "url": "/AntiGens/static/js/2.5adf4e03.chunk.js"
+  },
+  {
+    "revision": "6c5833db93cd7c0f813219a8bc75480b",
+    "url": "/AntiGens/static/js/2.5adf4e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcc0f649822dd05ec07",
+    "url": "/AntiGens/static/js/main.d49affd1.chunk.js"
+  },
+  {
+    "revision": "e31eed800191666b439e",
+    "url": "/AntiGens/static/js/runtime-main.856241b3.js"
+  }
+]);
