e82bc587614cbff45501d18b9b9ea7f2ed861b10
Minty <kinostl@gmail.com>
2020-06-11 03:31:50 -0400

    Added columns of nonsense

--- web/src/Antigens/create.js
+++ web/src/Antigens/create.js
@@ -6,7 +6,9 @@ import {
   Box,
   Hero,
   Heading,
-  Form
+  Form,
+  Columns,
+  Content
 } from 'react-bulma-components';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import arrayMove from 'array-move';
@@ -14,12 +16,12 @@ import arrayMove from 'array-move';
 function App() {
   const [stats, setStats] = useState([
     {
-      'stat': 'Body',
-      'color': '#f14668'
-    },
-    {
       'stat': 'Mind',
       'color': '#3298dc'
+    },
+    {
+      'stat': 'Body',
+      'color': '#f14668'
     },
     {
       'stat': 'Soul',
@@ -37,50 +39,68 @@ function App() {
               <Form.Input type="text" placeholder="Sugarsnap" />
             </Form.Control>
           </Form.Field>
-          <Form.Field>
-            <Form.Label>Stats</Form.Label>
-            <Form.Control>
-              <DragDropContext onDragEnd={(res) => {
-                if (!res.destination) {
-                  return;
-                }
+          <Columns>
+            <Columns.Column>
+              <Form.Field>
+                <Form.Label>Stats</Form.Label>
+                <Form.Control>
+                  <DragDropContext onDragEnd={(res) => {
+                    if (!res.destination) {
+                      return;
+                    }
 
-                setStats(
-                  arrayMove(stats, res.source.index, res.destination.index)
-                )
-              }}>
-                <Droppable droppableId="droppable">
-                  {(provided, snapshot) => (
-                    <div
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                    >
-                      {stats.map((item, index) => (
-                        <Draggable key={item.stat} draggableId={item.stat} index={index}>
-                          {(provided, snapshot) => (
-                            <div
-                              ref={provided.innerRef}
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                            >
-                              <Box
-                                style={{
-                                  backgroundColor: item.color,
-                                  color:'white'
-                                }}>
-                                {item.stat}
-                              </Box>
-                            </div>
-                          )}
-                        </Draggable>
-                      ))}
-                      {provided.placeholder}
-                    </div>
-                  )}
-                </Droppable>
-              </DragDropContext>
-            </Form.Control>
-          </Form.Field>
+                    setStats(
+                      arrayMove(stats, res.source.index, res.destination.index)
+                    )
+                  }}>
+                    <Droppable droppableId="droppable">
+                      {(provided, snapshot) => (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                        >
+                          {stats.map((item, index) => (
+                            <Draggable key={item.stat} draggableId={item.stat} index={index}>
+                              {(provided, snapshot) => (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  <Box
+                                    style={{
+                                      backgroundColor: item.color,
+                                      color: 'white'
+                                    }}>
+                                    {item.stat}
+                                  </Box>
+                                </div>
+                              )}
+                            </Draggable>
+                          ))}
+                          {provided.placeholder}
+                        </div>
+                      )}
+                    </Droppable>
+                  </DragDropContext>
+                </Form.Control>
+              </Form.Field>
+            </Columns.Column>
+            <Columns.Column>
+              <Form.Label>Scores</Form.Label>
+              <Content>
+                Stuff stuff
+              </Content>
+
+            </Columns.Column>
+            <Columns.Column>
+              <Form.Label>Calculations</Form.Label>
+              <Content>
+                Stuff stuff
+              </Content>
+
+            </Columns.Column>
+          </Columns>
         </Container>
       </Section>
     </div>
